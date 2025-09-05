4970410371d584f0a06bfa1995967cbda126ffa9
kecan <1905537268@qq.com>
2025-09-05 09:58:32 +0800

    Add IAave interface

---
+++ contracts/contracts/interfaces/IAave.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IAave {
+    function supply(
+        address asset,
+        uint256 amount,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external;
+
+    function withdraw(
+        address asset,
+        uint256 amount,
+        address to
+    ) external returns (uint256);
+
+    function getBalance(address asset, address user) external view returns (uint256);
+}
